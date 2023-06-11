9fcbaeda4ac8637e9cd0e9051d2cdf87ff4240a5
smajaas <smajaas@gmail.com>
2023-06-11 12:49:07 +0530

    setup cart items

---
+++ final/src/cartItems.js
@@ -0,0 +1,31 @@
+const cartItems = [
+  {
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Samsung Galaxy S23 Ultra',
+    price: '699.99',
+    img: 'https://m.media-amazon.com/images/I/61VfL-aiToL._SX679_.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recB6qcHPxb62YJ75',
+    title: 'google pixel 7 Pro',
+    price: '499.99',
+    img: 'https://lh3.googleusercontent.com/9TmH0ll2soWq3cJPIut5fHXtSuH2HIShxrel2GYOnt0Lm7tYNWcKV7c7Zy3ZacdQ4hxCLxZb3IFLuC0rfksJ1SVg88HzdHZndh2u=rw-e365-w1440',
+    amount: 1,
+  },
+  {
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Apple Iphone 13 Pro',
+    price: '1399.99',
+    img: 'https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iPhone-13-Pro_iPhone-13-Pro-Max_09142021_inline.jpg.large.jpg',
+    amount: 1,
+  },
+  {
+    id: 'recwTo160XST3PIoW',
+    title: 'Samsung Galaxy A73',
+    price: '599.99 ',
+    img: 'https://fdn2.gsmarena.com/vv/bigpic/samsung-galaxy-a73-5g.jpg',
+    amount: 1,
+  },
+];
+export default cartItems;
